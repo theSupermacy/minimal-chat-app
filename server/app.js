ef58f9e01f0b8674a2875efe59f55801c247791a
Rahul Sinha <rahulsinha@Rahuls-MacBook-Pro.local>
2019-01-20 19:12:40 +0530

    added cors

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var cors = require('cors')
 var cookieParser = require('cookie-parser');
 var dotEnv = require('dotenv')
 
@@ -14,6 +15,7 @@ var app = express();
 
 app.set('view engine', 'jade');
 app.use(logger('dev'));
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
